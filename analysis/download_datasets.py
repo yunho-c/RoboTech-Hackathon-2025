8f34df0a267a29f88eff758402542c97d3f213cb
Yunho Cho <yhjc2692@gmail.com>
2025-03-29 21:45:42 -0400

    Update download_datasets.py

--- analysis/download_datasets.py
+++ analysis/download_datasets.py
@@ -1,10 +1,20 @@
+# NOTE: currently doesn't work if `dataset` directory exists already. TODO: fix
+
 import kagglehub
 import os
 
-if __name__ == "__main__":
-    path = kagglehub.dataset_download("dataclusterlabs/cardboard-object-detection") # cardboard dataset
+
+def move_dataset(path):
     script_dir = os.path.dirname(__file__)
-    os.rename(path, os.path.join(script_dir, "dataset")) # Move to dataset directory relative to script
+    os.rename(
+        path, os.path.join(script_dir, "dataset")
+    )  # Move to dataset directory relative to script
 
-    print("Path to dataset files:", path) # DEBUG
 
+if __name__ == "__main__":
+    path = kagglehub.dataset_download(
+        "dataclusterlabs/cardboard-object-detection"
+    )  # cardboard dataset
+    move_dataset(path)
+
+    # print("Path to dataset files:", path) # DEBUG
